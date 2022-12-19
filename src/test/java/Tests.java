c0de125f922abc887e93178ebe92655c682d8f51
Malak47 <malak.qeedan@hotmail.com>
2022-12-19 23:24:54 +0200

    finishing tests with JvmUtilities.

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,22 +1,30 @@
+import api.ConcreteMember;
+import api.GroupAdmin;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
 
 public class Tests {
+
     public static final Logger logger = LoggerFactory.getLogger(Tests.class);
 
-    // stub method to check external dependencies compatibility
     @Test
     public void test() {
-        String s1 = "Alice";
-        String s2 = "Bob";
+        // Create a GroupAdmin object and register two ConcreteMember objects
+        GroupAdmin groupAdmin = new GroupAdmin();
+        ConcreteMember member1 = new ConcreteMember();
+        ConcreteMember member2 = new ConcreteMember();
+        groupAdmin.register(member1);
+        groupAdmin.register(member2);
 
-        logger.info(() -> JvmUtilities.objectFootprint(s1));
-
-        logger.info(() -> JvmUtilities.objectFootprint(s1, s2));
-
-        logger.info(() -> JvmUtilities.objectTotalSize(s1));
-
-        logger.info(() -> JvmUtilities.jvmInfo());
+        // Print the JVM info and the memory stats for the GroupAdmin and ConcreteMember objects
+        logger.info(JvmUtilities::jvmInfo);
+        System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+        logger.info(() -> JvmUtilities.memoryStats(groupAdmin));
+        System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+        logger.info(() -> JvmUtilities.memoryStats(member1));
+        System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+        logger.info(() -> JvmUtilities.memoryStats(member2));
+        System.out.println("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
     }
 }
