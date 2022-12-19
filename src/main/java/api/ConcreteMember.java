1b833186d3d40640278e61886854eb89f0d619ec
Malak47 <malak.qeedan@hotmail.com>
2022-12-19 19:38:33 +0200

    implementing ConcreteMember

--- src/main/java/api/ConcreteMember.java
+++ src/main/java/api/ConcreteMember.java
@@ -3,8 +3,22 @@ package api;
 import observer.*;
 
 public class ConcreteMember implements Member {
+    private UndoableStringBuilder usb;
+
+    public UndoableStringBuilder getUsb() {
+        return usb;
+    }
+
+    public ConcreteMember() {
+        this.usb = new UndoableStringBuilder();
+    }
+
+    public ConcreteMember(UndoableStringBuilder usb) {
+        this.usb = usb;
+    }
+
     @Override
     public void update(UndoableStringBuilder usb) {
-
+        this.usb = usb;
     }
 }
